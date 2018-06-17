3116cf5155f29661cad8c0500b71c031b7d7341a
Raúl Marty <santor3medio@gmail.com>
2018-06-17 08:39:17 -0500

    #13. Add the carousel functionality to the TeamHigia section in the who-card-carousel.js file.

---
+++ src/js/who-card-carousel.js
@@ -0,0 +1,27 @@
+var whoCardCarousel = $('.who-card-carousel');
+
+whoCardCarousel.owlCarousel ({
+  loop: false,
+  nav: true,
+  dots: true,
+  navText: ['<img class="who-card-navigation" src="/img/icons/black-arrow-left.png" />', '<img class="who-work-navigation" src="/img/icons/black-arrow-right.png" />'],
+  margin: 50,
+  responsive:{
+    0:{
+      items:1
+    },
+    700:{
+      items: 1
+    },
+    880: {
+      items: 1
+    },
+    1000:{
+      items:1
+    }
+  }
+});
+
+whoCardCarousel.each(function(index) {
+  $(this).find('.owl-nav, .owl-dots').wrapAll("<div class='who-card-controls'></div>");
+});
