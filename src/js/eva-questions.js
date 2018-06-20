5fa0d2eb4f833001e92371545ebfee9fcd4402cd
Raúl Marty <santor3medio@gmail.com>
2018-06-20 11:03:57 -0500

    #21. Add preliminar script of the faq open and close for the eva view in the eva-questions.js

--- src/js/eva-questions.js
+++ src/js/eva-questions.js
@@ -1,24 +1,41 @@
-$('.eva-questions-question').click(function(e) {
-  e.preventDefault();
+// $('.eva-questions-question').click(function(e) {
+//   e.preventDefault();
 
-  var $this = $(this);
+//   var $this = $(this);
 
-  if ($this.next().hasClass('show')) {
-      $this.next().removeClass('show');
-      $this.next().slideUp(350);
+//   if ($this.next().hasClass('show')) {
+//       $this.next().removeClass('show');
+//       $this.next().slideUp(350);
 
-  } else {
-      $this.parent().parent().find('.eva-questions-content .eva-questions-answer-container').removeClass('show');
-      $this.parent().parent().find('.eva-questions-content .eva-questions-answer-container').slideUp(350);
-      $this.next().toggleClass('show');
-      $this.next().slideToggle(350);
+//   } else {
+//       $this.parent().parent().find('.eva-questions-content .eva-questions-answer-container').removeClass('show');
+//       $this.parent().parent().find('.eva-questions-content .eva-questions-answer-container').slideUp(350);
+//       $this.next().toggleClass('show');
+//       $this.next().slideToggle(350);
+//   }
+// });
+
+
+document.addEventListener("DOMContentLoaded", function(event) { 
+  var question = document.getElementsByClassName("eva-questions-question");
+  var answer = document.getElementsByClassName('eva-questions-answer-container');
+
+  function setClass(els, className, fnName) {
+    for (var i = 0; i < els.length; i++) {
+        els[i].classList[fnName](className);
+    }
   }
 
-   if ($this.hasClass('active')) {
-      $this.removeClass("active");
+  for (var i = 0; i < question.length; i++) {
+    question[i].onclick = function() {
+      var setClasses = !this.classList.contains('active');
+      setClass(question, 'active', 'remove');
+      setClass(answer, 'show', 'remove');
 
-  } else {
-    $this.parent().parent().find('.eva-questions-content').removeClass('show');
-    $this.toggleClass("active");
+      if (setClasses) {
+        this.classList.toggle("active");
+        this.nextElementSibling.classList.toggle("show");
+      }
+    }
   }
 });
