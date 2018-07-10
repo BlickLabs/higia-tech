238c139a76152c8b52e8f9c0f751f7c3cb4359de
Raúl Marty <santor3medio@gmail.com>
2018-07-10 13:30:28 -0500

    #50. Remove the classes allies-donors-carousel and allies-donors-content in allies-donors-collapsible.js

--- src/js/allies-donors-collapsible.js
+++ src/js/allies-donors-collapsible.js
@@ -3,16 +3,12 @@ $(function() {
    if (!$(this).hasClass("expanded")){
       $(".allies-donors-container").animate({height: '100%',},"slow");
       $(".allies-donors-fadeout").css({display: 'none',},"slow");
-      $(".allies-donors-carousel").animate({height: '100%',},"slow");
-      $(".allies-donors-content").animate({height: '100%',},"slow");
       $(".allies-donors-collapsible").css({backgroundImage: 'url("/img/icons/icon-circle-arrow-up.png")',},"slow");
       $(this).addClass("expanded");
    }
    else {
       $(".allies-donors-container").animate({height: '235px',},"slow");
       $(".allies-donors-fadeout").css({display: 'block',},"slow");
-      $(".allies-donors-carousel").animate({height: '235px',},"slow");
-      $(".allies-donors-content").animate({height: '235px',},"slow");
       $(".allies-donors-collapsible").css({backgroundImage: 'url("/img/icons/icon-circle-arrow.png")',},"slow");
       $(this).removeClass("expanded");
    }
