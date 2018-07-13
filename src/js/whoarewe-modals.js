896dc860bf86d263d6102261b0a26b5390a2f2a6
Raúl Marty <santor3medio@gmail.com>
2018-07-13 15:20:22 -0500

    #52. Change the indentation in the file whoarewe.js

--- src/js/whoarewe-modals.js
+++ src/js/whoarewe-modals.js
@@ -1,26 +1,24 @@
 $(function(){
   $('a[data-modal]').click(function(event) {
     $(this).modal();
-    
-		$(function() {
-			var pathLocation = window.location.pathname;
-			if(pathLocation === '/' || pathLocation === '/higia-tech/' || pathLocation === '/higia-tech/index' || pathLocation === '/index' || pathLocation === '/higia-tech/es/index.html' || pathLocation === '/es/index.html' || pathLocation === '/higia-tech/en/index.html' || pathLocation === '/en/index.html' || pathLocation === '/higia-tech/es/eva.html' || pathLocation === '/es/eva.html' || pathLocation === '/higia-tech/en/eva.html' || pathLocation === '/en/eva.html') {
-				if ($.modal.isActive()) {
-					var dataId = event.currentTarget.dataset.id;
-					var dataVideo = document.getElementById(dataId);
 
-					dataVideo.currentTime = 0;
-					dataVideo.play();
-				}
+    var pathLocation = window.location.pathname;
+    if(pathLocation === '/' || pathLocation === '/higia-tech/' || pathLocation === '/higia-tech/index' || pathLocation === '/index' || pathLocation === '/higia-tech/es/index.html' || pathLocation === '/es/index.html' || pathLocation === '/higia-tech/en/index.html' || pathLocation === '/en/index.html' || pathLocation === '/higia-tech/es/eva.html' || pathLocation === '/es/eva.html' || pathLocation === '/higia-tech/en/eva.html' || pathLocation === '/en/eva.html') {
+      if ($.modal.isActive()) {
+        var dataId = event.currentTarget.dataset.id;
+        var dataVideo = document.getElementById(dataId);
 
-				function log_modal_event(event, modal) {
-					var dataVideo = document.getElementById(dataId);
-					dataVideo.pause();
-				}
+        dataVideo.currentTime = 0;
+        dataVideo.play();
+      }
 
-				$(document).on($.modal.CLOSE, log_modal_event);
-			}
-		});
+      function log_modal_event(event, modal) {
+        var dataVideo = document.getElementById(dataId);
+        dataVideo.pause();
+      }
+
+      $(document).on($.modal.CLOSE, log_modal_event);
+    }
     return false;
   });
 });
